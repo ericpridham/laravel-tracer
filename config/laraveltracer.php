203bdbc6c18feb880106627445fcfd5a2a93e80f
Eric Pridham <eric.pridham@gmail.com>
2019-11-30 23:53:25 -0500

    Customize root span name.

--- config/laraveltracer.php
+++ config/laraveltracer.php
@@ -2,6 +2,7 @@
 
 return [
     'enabled' => env('LARAVEL_TRACER_ENABLED', false),
+    'rootName' => env('LARAVEL_TRACER_ROOT_NAME', 'app'),
     'honeycomb' => [
         'key' => env('HONEYCOMB_KEY'),
         'dataset' => env('HONEYCOMB_DATASET'),
